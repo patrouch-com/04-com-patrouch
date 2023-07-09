98dc87df8ae7143affe7b7c4a50e9e48c21a6b5e
Chef <cheftech@patraldo.dev>
2023-07-08 20:23:37 -0600

    accessing Cloudflare's D1 example

--- src/routes/api/users/+page.svelte
+++
@@ -1,2 +0,0 @@
-<h1>Users -- example accessing Cloudflare's D1 from a Sveltekit server endpoint</h1>
-<p>Visit <a href="https://github.com/elithrar/svelte-demo-d1#accessing-d1-from-a-server-endpoint"> to read the documentation</a></p>
